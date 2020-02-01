d9c6790bf97bb1e378f0dd7d50d7ab5c5376cac7
umidah <ajtparco@outlook.com>
2020-02-01 10:56:13 +0800

    Added 3rd Algo (forgot the name)

--- main.cpp
+++ main.cpp
@@ -4,15 +4,14 @@ int main(){
     float x = 1.23456789f;
     nummets eq1;
     cout << eq1.truncate(x) << endl;
-    eq1.coefficient0(2); //initialize a power 2 equation with 0 as the coefficients
-    eq1.setCoefficient(0, 4);//set x^0 coefficient to 4
-    cout << 1 << endl;
-    eq1.setCoefficient(1, 4);
-    cout << 2 << endl;
+    eq1.coefficient0(2);
+    eq1.setCoefficient(0,8);
+    eq1.setCoefficient(1,-6);
     eq1.setCoefficient(2, 1);
-    cout << 3 << endl;
-    //f(x) = x^2 + 4x + 4
-    eq1.secantAlgo(-1, 0, 2, 8);
+    eq1.secantAlgo(0, 1, 2, 8);
+    eq1.otherAlgo(1, 2, 8);
     eq1.printSecData();
+    cout << endl << endl;
+    eq1.printOtherData();
     return 0;
 }
